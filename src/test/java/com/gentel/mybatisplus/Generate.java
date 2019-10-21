8daddc2dba4b0149e3bc54112eeace333d691fb5
廖江涛 <liaojt@szfangle.com>
2019-10-21 19:03:47 +0800

    工程编译通过

---
+++ src/test/java/com/gentel/mybatisplus/Generate.java
@@ -0,0 +1,21 @@
+package com.gentel.mybatisplus;
+
+import com.gentel.mybatisplus.generate.MysqlGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Gentel
+ * @description 代码生成器测试
+ * @create 2019-10-21 14:14
+ */
+
+@Slf4j
+public class Generate {
+
+    @Test
+    void tbOrderDetail(){
+        MysqlGenerator mysqlGenerator = new MysqlGenerator();
+
+    }
+}
